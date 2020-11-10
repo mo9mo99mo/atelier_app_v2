728a41dced5a73a1d4e653c351f0dda4a44d9134
Ubuntu <ubuntu@ip-172-31-64-99.ec2.internal>
2020-11-10 03:56:14 +0000

    seeder files edit

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -19,24 +19,32 @@ class ProductSeeder extends Seeder
             'product_user_id' => '1',
             'product_pub_flg' => '0',
             'product_del_flg' => '1',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
             ],[
             'product_name' => '作品名001型染',
             'product_cap' => '作品名001の説明。色糊型染',
             'product_user_id' => '2',
             'product_pub_flg' => '0',
             'product_del_flg' => '1',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
             ],[
             'product_name' => '作品名002',
             'product_cap' => '作品名002の説明。シルクスクリーン',
             'product_user_id' => '2',
             'product_pub_flg' => '0',
             'product_del_flg' => '1',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
             ],[
             'product_name' => '作品名001',
             'product_cap' => '作品名001の説明。デッサン',
             'product_user_id' => '3',
             'product_pub_flg' => '0',
             'product_del_flg' => '1',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
             ],
         ]);
     }
